9e5dae3334a89b93a20daf78538b9cf6ea5f10ca
szymon-rogus <sszymek98@gmail.com>
2020-10-20 18:12:20 +0200

    added plot visualization

--- Lab1/Hu.py
+++ Lab1/Hu.py
@@ -2,10 +2,15 @@ from math import copysign, log10
 
 import cv2
 import sys
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3, 4, 5, 6, 7]
+y = []
 
 
 def hu_calculation():
     showLogTransformedHuMoments = True
+    counter = 0
 
     for i in range(1, len(sys.argv)):
 
@@ -21,10 +26,18 @@ def hu_calculation():
 
         for j in range(0, 7):
             if showLogTransformedHuMoments:
+                y.append(-1 * copysign(1.0, huMoments[j]) * log10(abs(huMoments[j])))
                 print("{:.5f}".format(-1 * copysign(1.0, huMoments[j]) * log10(abs(huMoments[j]))), end=' ')
             else:
                 print("{:.5f}".format(huMoments[j]), end=' ')
         print()
+        plt.plot(x, y, label=filename[7:9])
+        y.clear()
+        counter += 1
+        if counter % 6 == 0:
+            plt.legend()
+            plt.show()
+            plt.clf()
 
 
 if __name__ == "__main__":
